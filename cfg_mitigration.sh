c6d8e5adf63a0696aa9f2f41d36551c7f286a0c5
miceforrat <211250069@smail.nju.edu.cn>
2024-09-15 00:35:01 +0800

    finish sh add wd

--- cfg_mitigration.sh
+++ cfg_mitigration.sh
@@ -1,8 +1,9 @@
 files=$(ls -a $1 | grep -E '.[^.]+' | grep -vE "(.git|cfg_mitigration.sh)")
+wd=$(pwd)
 for file in `echo $files`; do
-	ln -s $1/$file ~/$file
+	ln -s $wd/$file ~/$file
 	if [ $? -ne 0 ]; then
 		mv ~/$file ~/cfg_bak_$file
-		ln -s $1/$file ~/$file
+		ln -s $wd/$file ~/$file
 	fi
 done
