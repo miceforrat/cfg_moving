9a997c1e40b67ab207c42a0df96fcabc0f1362b0
miceforrat <211250069@smail.nju.edu.cn>
2024-09-15 00:29:24 +0800

    finish sh add

--- cfg_mitigration.sh
+++ cfg_mitigration.sh
@@ -1,7 +1,8 @@
-files=$(ls -a . | grep -E '.[^.]+' | grep -vE "(.git|cfg_mitigration.sh)")
+files=$(ls -a $1 | grep -E '.[^.]+' | grep -vE "(.git|cfg_mitigration.sh)")
 for file in `echo $files`; do
-	ln -s $file ~/$file
+	ln -s $1/$file ~/$file
 	if [ $? -ne 0 ]; then
 		mv ~/$file ~/cfg_bak_$file
+		ln -s $1/$file ~/$file
 	fi
 done
